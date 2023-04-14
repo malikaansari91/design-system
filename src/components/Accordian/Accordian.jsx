12250f8589f41e6a423a8576223564693c189091
Malika Ansari <malikaansari91@gmail.com>
2023-04-14 20:03:25 +0530

    Testing new config

--- src/components/Accordian/Accordian.jsx
+++ src/components/Accordian/Accordian.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import AccordianItem from "./AccordianItem";
 import AccordianHeader from "./AccordianHeader";
 import AccordianContent from "./AccordianContent";
